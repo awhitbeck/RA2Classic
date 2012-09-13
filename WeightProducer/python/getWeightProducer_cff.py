e64d1e10cd6f3090a83a1653f84aef4909664993
mschrode <>
2012-09-13 13:07:03 +0000

    NNLO cross sections for W/ZJets samples

--- WeightProducer/python/getWeightProducer_cff.py
+++ WeightProducer/python/getWeightProducer_cff.py
@@ -1,4 +1,4 @@
-# $Id: getWeightProducer_cff.py,v 1.2 2012/08/28 17:12:30 mschrode Exp $
+# $Id: getWeightProducer_cff.py,v 1.3 2012/08/30 16:29:43 mschrode Exp $
 #
 # Returns a WeightProducer module that knows at runtime
 # which data sample is produced and thus, what weights
@@ -42,39 +42,39 @@ def getWeightProducer(fileName):
     if "WJetsToLNu_HT-250To300_8TeV-madgraph" in fileName and "Summer12-PU_S7_START52_V9-v1" in fileName:
         mcVersion = "Summer12"
         weightProducer.Method     = cms.string("Constant")
-        weightProducer.XS         = cms.double(48.01)
+        weightProducer.XS         = cms.double(57.26)
         weightProducer.NumberEvts = cms.double(1634582)         
     if "WJetsToLNu_HT-300To400_8TeV-madgraph" in fileName and "Summer12-PU_S7_START52_V9-v1" in fileName:
         mcVersion = "Summer12"
         weightProducer.Method     = cms.string("Constant")
-        weightProducer.XS         = cms.double(38.3)
+        weightProducer.XS         = cms.double(45.68)
         weightProducer.NumberEvts = cms.double(1698744)         
     if "WJetsToLNu_HT-400ToInf_8TeV-madgraph" in fileName and "Summer12-PU_S7_START52_V9-v1" in fileName:
         mcVersion = "Summer12"
         weightProducer.Method     = cms.string("Constant")
-        weightProducer.XS         = cms.double(25.22)
+        weightProducer.XS         = cms.double(30.08)
         weightProducer.NumberEvts = cms.double(1647807)         
 
     # For ZJets
     if "ZJetsToNuNu_50_HT_100_TuneZ2Star_8TeV_madgraph" in fileName and "Summer12-PU_S7_START52_V9-v1" in fileName:
         mcVersion = "Summer12"
         weightProducer.Method     = cms.string("Constant")
-        weightProducer.XS         = cms.double(381.2)
+        weightProducer.XS         = cms.double(452.75)
         weightProducer.NumberEvts = cms.double(4053786)         
     if "ZJetsToNuNu_100_HT_200_TuneZ2Star_8TeV_madgraph" in fileName and "Summer12-PU_S7_START52_V9-v1" in fileName:
         mcVersion = "Summer12"
         weightProducer.Method     = cms.string("Constant")
-        weightProducer.XS         = cms.double(160.3)
+        weightProducer.XS         = cms.double(190.39)
         weightProducer.NumberEvts = cms.double(4416646)         
-##     if "ZJetsToNuNu_200_HT_400_TuneZ2Star_8TeV_madgraph" in fileName and "Summer12-PU_S7_START52_V9-v1" in fileName:
-##         mcVersion = "Summer12"
-##         weightProducer.Method     = cms.string("Constant")
-##         weightProducer.XS         = cms.double(41.49)
-##         weightProducer.NumberEvts = cms.double()         
+    if "ZJetsToNuNu_200_HT_400_TuneZ2Star_8TeV_madgraph" in fileName and "Summer12-PU_S7_START52_V9-v3" in fileName:
+        mcVersion = "Summer12"
+        weightProducer.Method     = cms.string("Constant")
+        weightProducer.XS         = cms.double(49.28)
+        weightProducer.NumberEvts = cms.double(5066608)         
     if "ZJetsToNuNu_400_HT_inf_TuneZ2Star_8TeV_madgraph" in fileName and "Summer12-PU_S7_START52_V9-v1" in fileName:
         mcVersion = "Summer12"
         weightProducer.Method     = cms.string("Constant")
-        weightProducer.XS         = cms.double(5.274)
+        weightProducer.XS         = cms.double(6.26)
         weightProducer.NumberEvts = cms.double(1006928)         
 
     # For QCD
