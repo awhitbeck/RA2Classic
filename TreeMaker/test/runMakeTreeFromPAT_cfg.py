dc5e082eef3c08bb695ea356cd1f3bfa078e7a90
mschrode <>
2012-09-19 14:01:37 +0000

    give file name as argument

--- TreeMaker/test/runMakeTreeFromPAT_cfg.py
+++ TreeMaker/test/runMakeTreeFromPAT_cfg.py
@@ -1,5 +1,9 @@
-# $Id: runMakeTreeFromPAT_cfg.py,v 1.1 2012/08/02 14:37:01 mschrode Exp $
+# $Id: runMakeTreeFromPAT_cfg.py,v 1.2 2012/09/14 13:10:37 mschrode Exp $
+#
+# Expects a file name as argument e.g.
+# cmsRun runMakeTreeFromPAT_cfg.py /store/user/kheine/ZJetsToNuNu_200_HT_400_TuneZ2Star_8TeV_madgraph/RA2PreSelectionOnMC_200_HT_400_Summer12-PU_S7_START52_V9-v3_v1//956a76b9479f1eb39208c1bee6fa7dc2/RA2SkimsOnMC_125_3_W97.root
 
+import sys
 import FWCore.ParameterSet.Config as cms
 process = cms.Process("RA2EventSelection")
 
@@ -8,8 +12,8 @@ makeTreeFromPAT(process,
                 outFileName="RA2Selection",
                 useCHSJets=True,
                 NJetsMin=2,
-                HTMin=0.,
-                MHTMin=0.,
+                HTMin=500.,
+                MHTMin=200.,
                 reportEveryEvt=5000,
-                testFileName=["/store/user/kheine/ZJetsToNuNu_200_HT_400_TuneZ2Star_8TeV_madgraph/RA2PreSelectionOnMC_200_HT_400_Summer12-PU_S7_START52_V9-v3_v1//956a76b9479f1eb39208c1bee6fa7dc2/RA2SkimsOnMC_125_3_W97.root"],
+                testFileName=sys.argv[2],
                 numProcessedEvt=100)
