b75df2c5aaf9fc186d4adf16cf90fe4d11eb9ccd
kheine <>
2012-11-13 15:16:49 +0000

    new data configs for 535

--- QCDBkgRS/test/QCDSmearing_OnData_from_PAT_2012_cfg.py
+++ QCDBkgRS/test/QCDSmearing_OnData_from_PAT_2012_cfg.py
@@ -1,3 +1,22 @@
+###############################################################################
+import sys,os
+
+GlobalTagName = ""
+if hasattr(sys,"argv"):
+    for arg in sys.argv :
+        pairs = arg.split(',')
+        for pair in pairs:
+            vals = pair.split('=')
+            if len(vals) == 2 :
+                if vals[0] == "GlobalTag":
+                    GlobalTagName = vals[1]
+
+GlobalTagName += "::All"
+
+print "********** GlobalTag = "+GlobalTagName
+
+###############################################################################
+
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("RA2QCDSmearingClosure")
@@ -16,14 +35,14 @@ process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.source = cms.Source("PoolSource",
 	fileNames = cms.untracked.vstring(
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_312_1_l4U.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_195_1_Dhl.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_156_1_xny.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_321_1_IhY.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_230_1_bRi.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_67_1_olT.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_646_1_rR0.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_500_1_agk.root',              
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_215_1_7bE.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_28_1_EOW.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_215_1_7bE.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_27_1_LQC.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_32_1_HnL.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_47_1_Q7T.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_72_1_DcZ.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_82_1_zkG.root'           
 	)
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000 ) )
@@ -31,9 +50,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000 ) )
 ###############################################################################
 ## Global tags and geometry
 # default configuration with frontier conditions
-process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.Geometry.GeometryIdeal_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('GR_R_52_V9D::All')
+process.GlobalTag.globaltag = cms.string(GlobalTagName)
 ###############################################################################
 
 ###############################################################################
@@ -49,7 +68,7 @@ process.load("RA2Classic.QCDBkgRS.qcdbkgrs_cfi")
 ###############################################################################
 #process.QCDfromSmearing.SmearingFile = '/afs/naf.desy.de/user/k/kriheine/Resolution/MCJetResolution_Summer11_QCD_Pt_15to3000_TuneZ2_Flat_7TeV_pythia6_fine_v2.root'
 #process.QCDfromSmearing.SmearingFile = '/afs/naf.desy.de/user/k/kriheine/Resolution/MCResolutions_Fall11_QCD_Pt-15to3000_TuneZ2_7TeV_pythia6_fine.root'
-process.QCDfromSmearing.SmearingFile = '/afs/naf.desy.de/user/k/kriheine/Resolution/MCJetResolution_Summer12_QCD_Pt_15to3000_TuneZ2_Flat_8TeV_pythia6_withCHS_withPUReweighting_pixelcorr.root'
+process.QCDfromSmearing.SmearingFile = '/afs/naf.desy.de/user/k/kriheine/Resolution/MCJetResolution_Summer12_QCD_Pt_15to3000_TuneZ2star_Flat_8TeV_pythia6_withCHS_withPUReweighting.root'
 process.QCDfromSmearing.BProbabilityFile = '/afs/naf.desy.de/user/k/kriheine/Resolution/BJetProbability.root'
 process.QCDfromSmearing.jetCollection = 'patJetsPF'
 process.QCDfromSmearing.uncertaintyName = ''
@@ -90,10 +109,11 @@ process.QCDfromSmearing.AdditionalSmearing	= cms.vdouble(1.052,1.052,1.052,1.052
 
 process.QCDfromSmearing.absoluteTailScaling = False
 process.QCDfromSmearing.SmearedJetPt = 0.
-process.QCDfromSmearing.RebalanceJetPt = 13.
+process.QCDfromSmearing.RebalanceJetPt = 15.
 process.QCDfromSmearing.RebalanceMode = 'MHThigh'
 process.QCDfromSmearing.weightName = 'prescaleweightProducer:weight'
 process.QCDfromSmearing.ControlPlots = True
+process.QCDfromSmearing.IsData = True 
 process.QCDfromSmearing.Ntries = 100
 process.QCDfromSmearing.MHTcut_low = cms.double(200.)
 process.QCDfromSmearing.MHTcut_medium = cms.double(350.)
@@ -115,6 +135,20 @@ process.prescaleweightProducer.PrescaleCut = -1
 ###############################################################################
 
 ###############################################################################
+# Filters
+###############################################################################
+from RecoMET.METFilters.jetIDFailureFilter_cfi import jetIDFailure
+process.PBNRFilter = jetIDFailure.clone(
+    JetSource = cms.InputTag('patJetsPF'),
+    MinJetPt      = cms.double(0.0),
+    taggingMode   = cms.bool(False)
+    )
+from RecoMET.METFilters.multiEventFilter_cfi import multiEventFilter
+process.HCALLaserEvtFilterList2012 = multiEventFilter.clone(
+    file        = cms.FileInPath('EventFilter/HcalRawToDigi/data/AllBadHCALLaser.txt'),
+    taggingMode = cms.bool(False)
+    )
+
 process.load("SandBox.Skims.filterBoolean_cfi")
 process.RA2_HBHENoiseFilterRA2    = process.booleanFilter.clone()
 process.RA2_HBHENoiseFilterRA2.ResultSource = cms.InputTag("HBHENoiseFilterRA2","HBHENoiseFilterResult","PAT")
@@ -132,14 +166,17 @@ process.RA2_EcalTPFilter          = process.booleanFilter.clone()
 process.RA2_EcalTPFilter.ResultSource = cms.InputTag("ra2EcalTPFilter")
 process.RA2_EcalBEFilter          = process.booleanFilter.clone()
 process.RA2_EcalBEFilter.ResultSource = cms.InputTag("ra2EcalBEFilter")
-process.HcalLaserEventFilter      = process.booleanFilter.clone()
-process.HcalLaserEventFilter.ResultSource = cms.InputTag("hcalLaserEventFilter")
+#process.HcalLaserEventFilter      = process.booleanFilter.clone()
+#process.HcalLaserEventFilter.ResultSource = cms.InputTag("hcalLaserEventFilter")
 process.EEBadScFilter             = process.booleanFilter.clone()
 process.EEBadScFilter.ResultSource = cms.InputTag("eeBadScFilter")
+#process.EcalLaserFilter           = process.booleanFilter.clone()
+#process.EcalLaserFilter.ResultSource= cms.InputTag("ecalLaserCorrFilter")
 ###############################################################################
 
 ###############################################################################
-process.load('SandBox.Skims.RA2HT_cff')
+# Lepton Veto
+###############################################################################
 process.load('SandBox.Skims.RA2Leptons_cff')
 ###############################################################################
 
@@ -157,18 +194,17 @@ process.dump   = cms.EDAnalyzer("EventContentAnalyzer")
 
 process.prediction = cms.Path(
 #	process.dump *
-#   process.htPFFilter *    # to be used for standard jets
-   process.htPFchsFilter *  # to be used for chs jets
    process.RA2_HBHENoiseFilterRA2 *
    process.RA2_beamHaloFilter *
-   process.RA2_eeNoiseFilter *
+#   process.RA2_eeNoiseFilter *
    process.RA2_trackingFailureFilter *
    process.RA2_inconsistentMuons *
    process.RA2_greedyMuons *
    process.RA2_EcalTPFilter *
    process.RA2_EcalBEFilter *
-   process.HcalLaserEventFilter *
    process.EEBadScFilter *
+   process.HCALLaserEvtFilterList2012 *
+   process.PBNRFilter *
    process.ra2PFMuonVeto *
    process.ra2ElectronVeto *
 	process.prescaleweightProducer *
--- QCDBkgRS/test/Selection_OnData_from_PAT_2012_cfg.py
+++ QCDBkgRS/test/Selection_OnData_from_PAT_2012_cfg.py
@@ -16,14 +16,14 @@ process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.source = cms.Source("PoolSource",
 	fileNames = cms.untracked.vstring(
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_312_1_l4U.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_195_1_Dhl.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_156_1_xny.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_321_1_IhY.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_230_1_bRi.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_67_1_olT.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_646_1_rR0.root',
-           '/store/user/kheine/HT/RA2PreSelectionOnData_Run2012A_HT_PromptReco-v1_v7/e99cf838fe40ef4081c91a81748e6000/RA2SkimsOnData_500_1_agk.root', 
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_215_1_7bE.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_28_1_EOW.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_215_1_7bE.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_27_1_LQC.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_32_1_HnL.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_47_1_Q7T.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_72_1_DcZ.root',
+           '/store/user/mschrode/HT/RA2PreSelection_Run2012A-13Jul2012-v1_V2/528d417548fa47de754292e17c1b0d17/RA2Skim_82_1_zkG.root'
 	)
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000 ) )
@@ -31,9 +31,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000 ) )
 ###############################################################################
 ## Global tags and geometry
 # default configuration with frontier conditions
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('GR_R_52_V9D::All')
+#process.load("Configuration.StandardSequences.Geometry_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = cms.string('GR_R_52_V9D::All')
 ###############################################################################
 
 ###############################################################################
@@ -41,6 +41,20 @@ process.TFileService = cms.Service("TFileService",fileName = cms.string("Selecti
 ###############################################################################
 
 ###############################################################################
+# Filters
+###############################################################################
+from RecoMET.METFilters.jetIDFailureFilter_cfi import jetIDFailure
+process.PBNRFilter = jetIDFailure.clone(
+    JetSource = cms.InputTag('patJetsPF'),
+    MinJetPt      = cms.double(30.0),
+    taggingMode   = cms.bool(False)
+    )
+from RecoMET.METFilters.multiEventFilter_cfi import multiEventFilter
+process.HCALLaserEvtFilterList2012 = multiEventFilter.clone(
+    file        = cms.FileInPath('EventFilter/HcalRawToDigi/data/AllBadHCALLaser.txt'),
+    taggingMode = cms.bool(False)
+    )
+
 process.load("SandBox.Skims.filterBoolean_cfi")
 process.RA2_HBHENoiseFilterRA2    = process.booleanFilter.clone()
 process.RA2_HBHENoiseFilterRA2.ResultSource = cms.InputTag("HBHENoiseFilterRA2","HBHENoiseFilterResult","PAT")
@@ -58,14 +72,15 @@ process.RA2_EcalTPFilter          = process.booleanFilter.clone()
 process.RA2_EcalTPFilter.ResultSource = cms.InputTag("ra2EcalTPFilter")
 process.RA2_EcalBEFilter          = process.booleanFilter.clone()
 process.RA2_EcalBEFilter.ResultSource = cms.InputTag("ra2EcalBEFilter")
-process.HcalLaserEventFilter      = process.booleanFilter.clone()
-process.HcalLaserEventFilter.ResultSource = cms.InputTag("hcalLaserEventFilter")
+#process.HcalLaserEventFilter      = process.booleanFilter.clone()
+#process.HcalLaserEventFilter.ResultSource = cms.InputTag("hcalLaserEventFilter")
 process.EEBadScFilter             = process.booleanFilter.clone()
 process.EEBadScFilter.ResultSource = cms.InputTag("eeBadScFilter")
+#process.EcalLaserFilter           = process.booleanFilter.clone()
+#process.EcalLaserFilter.ResultSource= cms.InputTag("ecalLaserCorrFilter")
 ###############################################################################
 
 ###############################################################################
-process.load('SandBox.Skims.RA2HT_cff')
 process.load('SandBox.Skims.RA2Leptons_cff')
 ###############################################################################
 
@@ -97,18 +112,17 @@ process.load("HLTrigger.HLTcore.hltEventAnalyzerAOD_cfi")
 process.dump   = cms.EDAnalyzer("EventContentAnalyzer")
 
 process.data = cms.Path(
- #  process.htPFFilter *    # to be used for standard jets
-   process.htPFchsFilter *     # to be used for chs jets 
    process.RA2_HBHENoiseFilterRA2 *
    process.RA2_beamHaloFilter *
-   process.RA2_eeNoiseFilter *
+#   process.RA2_eeNoiseFilter *
    process.RA2_trackingFailureFilter *
    process.RA2_inconsistentMuons *
    process.RA2_greedyMuons *
    process.RA2_EcalTPFilter *
    process.RA2_EcalBEFilter *
-   process.HcalLaserEventFilter *
    process.EEBadScFilter *
+   process.HCALLaserEvtFilterList2012 *
+   process.PBNRFilter *
    process.ra2PFMuonVeto *
    process.ra2ElectronVeto * 
    process.produceRA2JetsPFCHS *
