491fefb3cd02e773b07e0cfdead23cd1ab4dfacf
mschrode <>
2012-11-15 18:17:08 +0000

    Adapt to 53X recipe

--- MCResolutions/test/mcresolutions_PAT_cfg.py
+++ MCResolutions/test/mcresolutions_PAT_cfg.py
@@ -1,16 +1,73 @@
 #
 #  SUSY-PAT configuration file adapted for RA2 workflow
 #
-#  PAT configuration for the SUSY group - 52X series
+#  PAT configuration for the SUSY group - 53X series
 #  More information here:
 #  https://twiki.cern.ch/twiki/bin/view/CMS/SusyPatLayer1DefV12
 #
+#  Run as
+#  'cmsRun mcresolutions_PAT_cfg.py <command-line key>=<value>,<command-line key>=value,...
 
-# Starting with a skeleton process which gets imported with the following line
+
+#-- Global Parameters: Set Via Command-Line Arguments -------------------------
+# default values
+runOnMC          = True                 # command-line key: is_mc
+dataSetName      = ""                   # command-line key: data_set
+globalTag        = ""                   # command-line key: global_tag
+lumi             = 1000                 # command-line key: lumi
+doPUReweighting  = False                # command-line key: pu_reweighting
+mcResoJetTag     = "patJetsPF"          # command-line key: jet_tag
+mcResoFileName   = "MCResoDefault.root" # command-line key: out_name
+
+# parse command line arguments
+import sys,os
+if hasattr(sys,"argv") :
+    for args in sys.argv :
+        arg = args.split(',')
+        for val in arg:
+            val = val.split('=')
+            if len(val)==2 :
+                if val[0]=="is_mc":
+                    if val[1]=="True":
+                        runOnMC=True
+                    else:
+                        runOnMC=False
+                elif val[0]=="data_set":
+                    dataSetName=val[1]
+                elif val[0]=="global_tag":
+                    globalTag=val[1]
+                    globalTag+="::All"
+                elif val[0]=="lumi":
+                    lumi=val[1]
+                elif val[0]=="pu_reweighting":
+                    doPUReweighting=val[1]
+                elif val[0]=="jet_tag":
+                    mcResoJetTag=val[1]
+                elif val[0]=="out_name":
+                    mcResoFileName=val[1]
+
+
+
+print "*** JOB SETUP ****************************************************"
+print "  is_mc          : "+str(runOnMC)
+print "  data_set       : "+dataSetName
+print "  global_tag     : "+globalTag
+print "  lumi           : "+str(lumi)
+print "  pu_reweighting : "+str(doPUReweighting)
+print "  jet_tag        : "+mcResoJetTag
+print "  out_name       : "+mcResoFileName
+print "******************************************************************"
+
+
+
+
+#-- Import the Default Process ------------------------------------------------
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
-runningOnMC = True 
-#runningOnMC = False
+
+#-- Global Tag ----------------------------------------------------------------
+process.GlobalTag.globaltag = globalTag
+
 
 #-- Message Logger ------------------------------------------------------------
 process.MessageLogger.categories.append('PATSummaryTables')
@@ -18,41 +75,30 @@ process.MessageLogger.cerr.PATSummaryTables = cms.untracked.PSet(
     limit = cms.untracked.int32(10),
     reportEvery = cms.untracked.int32(1)
     )
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
 
 
 #-- Input Source --------------------------------------------------------------
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring('/store/mc/Summer12/QCD_Pt-15to3000_TuneZ2_Flat_8TeV_pythia6/AODSIM/PU_S7_START52_V9-v1/0000/2C5FB809-F697-E111-87BB-0025901D4AF0.root')
+                            )
+process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 process.maxEvents.input = 100
 
-# Due to problem in production of LM samples: same event number appears multiple times
-process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
-############################# START SUSYPAT specifics ####################################
+#-- SUSY PAT Specifics --------------------------------------------------------
 from PhysicsTools.Configuration.SUSY_pattuple_cff import addDefaultSUSYPAT, getSUSY_pattuple_outputCommands
 
-hltMenu = 'HLT'
-
 theJetColls = ['AK5PF']
-
 jetMetCorr = ['L1FastJet', 'L2Relative', 'L3Absolute']
-if runningOnMC == False: jetMetCorr.append('L2L3Residual')  
 
-process.GlobalTag.globaltag = "START52_V11C::All"
-if runningOnMC == False:
-    process.GlobalTag.globaltag = "GR_R_52_V9D::All"
-
-process.source = cms.Source("PoolSource",
-   fileNames = cms.untracked.vstring('/store/mc/Summer12/QCD_Pt-15to3000_TuneZ2_Flat_8TeV_pythia6/AODSIM/PU_S7_START52_V9-v1/0000/2C5FB809-F697-E111-87BB-0025901D4AF0.root')
-)
-if runningOnMC == False:
-    process.source = cms.Source("PoolSource",
-     fileNames = cms.untracked.vstring('/store/data/Run2012B/HTMHT/AOD/PromptReco-v1/000/194/052/9A12DE52-699E-E111-BC9F-5404A638869E.root')
-    )
-
-# Due to problem in production of LM samples: same event number appears multiple times
-process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
-
-addDefaultSUSYPAT(process,mcInfo=runningOnMC,HLTMenu=hltMenu,jetMetCorrections=jetMetCorr,mcVersion='',theJetNames=theJetColls, doSusyTopProjection=False)
+addDefaultSUSYPAT(process,
+                  mcInfo=runOnMC,
+                  HLTMenu='HLT',
+                  jetMetCorrections=jetMetCorr,
+                  mcVersion='',
+                  theJetNames=theJetColls,
+                  doSusyTopProjection=False)
 
 # Remove the PAT cleaning and filtering sequences
 process.patDefaultSequence.remove(process.selectedPatCandidates)
@@ -74,14 +120,6 @@ process.pfPileUpPF.checkClosestZVertex = cms.bool(False)
 # do not remove muons and electrons from the jet clustering input
 process.pfIsolatedElectronsPF.isolationCut = -1.0
 process.pfIsolatedMuonsPF.isolationCut = -1.0
-
-# do not remove taus from the jet collection
-process.pfTausPF.discriminators = cms.VPSet()
-process.pfUnclusteredTausPF = process.pfTausPF.clone(
-    cut = cms.string("pt < 0")
-    )
-process.pfTauSequencePF.replace(process.pfTausPF, process.pfTausPF+ process.pfUnclusteredTausPF)
-process.pfNoTauPF.topCollection = "pfUnclusteredTausPF"
 
 # make loose clones of the original electron collection
 process.pfRelaxedElectronsPF = process.pfIsolatedElectronsPF.clone()
@@ -105,63 +143,80 @@ process.pfMuonSequencePF.replace(process.pfIsolatedMuonsPF,
                                  process.pfIsolatedMuonsPF +
                                  process.pfRelaxedMuonsPF)
 
-
-
-#------------------------------------------------------------------------------
-
-#SUSY_pattuple_outputCommands = getSUSY_pattuple_outputCommands( process )
+# do not remove taus from the jet collection
+process.pfTausPF.discriminators = cms.VPSet()
+process.pfUnclusteredTausPF = process.pfTausPF.clone(
+    cut = cms.string("pt < 0")
+    )
+process.pfTauSequencePF.replace(process.pfTausPF, process.pfTausPF+ process.pfUnclusteredTausPF)
+process.pfNoTauPF.topCollection = "pfUnclusteredTausPF"
 
 # overwrite default output content
 from SandBox.Skims.RA2Content_cff import getRA2PATOutput
 process.out.outputCommands = getRA2PATOutput(process)
 process.out.dropMetaData = cms.untracked.string('DROPPED')
-############################## END SUSYPAT specifics ####################################
-
-#-- HLT selection ------------------------------------------------------------
-import HLTrigger.HLTfilters.hltHighLevel_cfi as hlt
-hltSelection=''
-#if options.hltSelection:
-if hltSelection:
-    process.hltFilter = hlt.hltHighLevel.clone(
-        HLTPaths = cms.vstring(hltSelection),
-        TriggerResultsTag = cms.InputTag("TriggerResults","",hltMenu),
-        throw = False
-    )
-    process.susyPatDefaultSequence.replace(process.eventCountProducer, process.eventCountProducer * process.hltFilter)
 
 
-#-- check RA2 recipe here ------------------------------------------------------------
-process.prefilterCounter        = cms.EDProducer("EventCountProducer")
-process.postStdCleaningCounter  = cms.EDProducer("EventCountProducer")
-
-#-- Output module configuration -----------------------------------------------
+#-- RA2 filtering -------------------------------------------------------------
 process.load('SandBox.Skims.RA2Objects_cff')
+process.load('SandBox.Skims.RA2Leptons_cff')
 process.load('SandBox.Skims.RA2Selection_cff')
 process.load('SandBox.Skims.RA2Cleaning_cff')
-
-## please comment this block to remove tagging mode of
-##filters and reject events failing any of following filters
-#process.eeNoiseFilter.taggingMode         = True
-#process.trackingFailureFilter.taggingMode = True
-#process.beamHaloFilter.taggingMode        = True
-#process.ra2NoiseCleaning.remove(process.HBHENoiseFilter)
-#process.inconsistentMuons.taggingMode     = True
-#process.greedyMuons.taggingMode           = True
-#process.ra2EcalTPFilter.taggingMode       = True
-#process.ra2EcalBEFilter.taggingMode       = True
-#process.hcalLaserEventFilter.taggingMode  = True
-#process.eeBadScFilter.taggingMode         = True
-
 process.load("SandBox.Skims.provInfoMuons_cfi")
 process.load("SandBox.Skims.provInfoElectrons_cfi")
 
-###############################################################################
-process.load("RA2Classic.MCResolutions.mcresolutions_cfi")
-process.MCResolutions.jetTag = 'patJetsPF' #Without CHS: patJetsAK5PF. With CHS: patJetsPF
+# adjust the ra2NoiseCleaning sequence
+process.trackingFailureFilter.taggingMode = False
+process.beamHaloFilter.taggingMode        = False
+process.inconsistentMuons.taggingMode     = False
+process.greedyMuons.taggingMode           = False
+process.ra2EcalTPFilter.taggingMode       = False
+process.ra2EcalBEFilter.taggingMode       = False
+process.hcalLaserEventFilter.taggingMode  = False
+process.eeBadScFilter.taggingMode         = False
+process.ecalLaserCorrFilter.taggingMode   = False
 
-process.MCResolutions = process.MCResolutions.clone()
-process.MCResolutions.fileName = 'MCJetResolution_MadGraph1000ToInf_withCHS_withoutPUReweighting.root'
+process.ra2NoiseCleaning.remove(process.eeNoiseFilter)
+process.ra2NoiseCleaning.remove(process.HBHENoiseFilter)
+
+from RecoMET.METFilters.jetIDFailureFilter_cfi import jetIDFailure
+process.PBNRFilter = jetIDFailure.clone(
+    JetSource = cms.InputTag(mcResoJetTag), # These are the jets for the resolution determination
+    MinJetPt      = cms.double(0.0),
+    taggingMode   = cms.bool(False)
+    )
+from RecoMET.METFilters.multiEventFilter_cfi import multiEventFilter
+process.HCALLaserEvtFilterList2012 = multiEventFilter.clone(
+    file        = cms.FileInPath('EventFilter/HcalRawToDigi/data/AllBadHCALLaser.txt'),
+    taggingMode = cms.bool(False)
+    )
+process.ra2NoiseCleaning += process.PBNRFilter
+process.ra2NoiseCleaning += process.HCALLaserEvtFilterList2012
+
+process.LeptonVeto = cms.Sequence(
+    process.ra2PFMuonVeto *
+    process.ra2ElectronVeto
+    )
+
+# The final cleaning sequence
+process.RA2CleaningSequence = cms.Sequence(
+    process.susyPatDefaultSequence *
+    process.ra2StdCleaning *
+    process.ra2Objects * process.provInfoMuons * process.provInfoElectrons *
+    process.ra2PostCleaning *
+    process.LeptonVeto
+    )
+
+
+
 ###############################################################################
+from RA2Classic.MCResolutions.mcresolutions_cfi import MCResolutions
+process.MCReso = MCResolutions.clone(
+    jetTag = mcResoJetTag,
+    fileName = mcResoFileName
+    )
+###############################################################################
+
 
 ###############################################################################
 # Jet parton matching
@@ -170,34 +225,17 @@ process.MCResolutions.fileName = 'MCJetResolution_MadGraph1000ToInf_withCHS_with
 process.patJetPartonAssociation.coneSizeToAssociate = 0.5
 ###############################################################################
 
+
 ###############################################################################
 # Weight producer
 ###############################################################################
-process.load("RA2Classic.WeightProducer.weightProducer_cfi")
-process.weightProducer.weightName       = 'weight'
-
-## QCD_Pt-15to3000_TuneZ2_Flat_8TeV_pythia6
-#process.weightProducer.Method          = 'PtHat'
-#process.weightProducer.Exponent        = -4.5
-#process.weightProducer.XS              = 2.99913994e+10
-#process.weightProducer.NumberEvts      = 9998154
-#process.weightProducer.Lumi            = 1000
-#process.weightProducer.weight          = -1.0
-
-## MadGraph 
-process.weightProducer.Method           = 'Constant'
-process.weightProducer.Lumi             = 1000
-#process.weightProducer.NumberEvts       = 27057349 #250To500
-#process.weightProducer.XS               = 276000.0
-#process.weightProducer.NumberEvts      = 32255694 #500To1000
-#process.weightProducer.XS               = 8426.0
-process.weightProducer.NumberEvts      = 13879218 #1000ToInf
-process.weightProducer.XS               = 204.0
-
-process.weightProducer.FileNamePUDataDistribution = 'NONE'
-#process.weightProducer.FileNamePUDataDistribution = 'RA2Classic/AdditionalInputFiles/DataPileupHistogram_RA2Summer12_190456-196531_8TeV_PromptReco_WOLowPU.root'
-process.weightProducer.PU = 2 # 0==Flat10, 1==Fall11, 2==Summer12
+from RA2Classic.WeightProducer.getWeightProducer_cff import getWeightProducer
+process.WeightProducer = getWeightProducer(dataSetName)
+process.WeightProducer.Lumi = lumi
+if not doPUReweighting:
+    process.WeightProducer.FileNamePUDataDistribution = 'NONE'
 ###############################################################################
+
 
 ###############################################################################
 #-- process.outpath is set in PhysicsTools.PatAlgos.patTemplate_cfg
@@ -206,24 +244,9 @@ del process.outpath
 #del process.ppf
 ###############################################################################
 
-# an example sequence to create skimmed susypat-tuples
-process.cleanpatseq = cms.Sequence(
-                      process.susyPatDefaultSequence *
-                      #process.prefilterCounter *
-                      process.ra2StdCleaning *
-                      #process.postStdCleaningCounter *
-                      process.ra2Objects * process.provInfoMuons * process.provInfoElectrons *
-                      process.ra2PostCleaning 
-                      #process.ra2FullPFchsSelectionNoMHT 
-                      #process.mhtchsPFFilter
-                      )
 
 process.p = cms.Path(
-	process.cleanpatseq *
-	process.weightProducer *
-	process.MCResolutions
-)
-
-file=open("mcresolutions_PAT_resolved.py","w")
-file.write(str(process.dumpPython()))
-file.close()
+    process.RA2CleaningSequence *
+    process.WeightProducer *
+    process.MCReso
+    )
