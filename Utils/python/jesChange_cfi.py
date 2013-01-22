fc3f28cac31908347c27e6247d87f0999f7c5394
mschrode <>
2013-01-22 18:04:50 +0000

    fix correctors syntax

--- Utils/python/jesChange_cfi.py
+++ Utils/python/jesChange_cfi.py
@@ -1,13 +1,13 @@
-# $Id: $
+# $Id: jesChange_cfi.py,v 1.1 2012/12/18 16:59:16 mschrode Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 # Define CHS correctors
 from JetMETCorrections.Configuration.DefaultJEC_cff import *
-ak5PFchsL1Fastjet           = ak5PFL1Fastjet.clone( correctors = cms.vstring('ak5PFchsL1Fastjet') )
-ak5PFchsL2Relative          = ak5PFL2Relative.clone( correctors = cms.vstring('ak5PFchsL2Relative') )
-ak5PFchsL3Absolute          = ak5PFL3Absolute.clone( correctors = cms.vstring('ak5PFchsL2Absolute') )
-ak5PFchsResidual            = ak5PFResidual.clone( correctors = cms.vstring('ak5PFchsResidual') )
+ak5PFchsL1Fastjet           = ak5PFL1Fastjet.clone ( algorithm = cms.string('AK5PFchs') )
+ak5PFchsL2Relative          = ak5PFL2Relative.clone( algorithm = cms.string('AK5PFchs') )
+ak5PFchsL3Absolute          = ak5PFL3Absolute.clone( algorithm = cms.string('AK5PFchs') )
+ak5PFchsResidual            = ak5PFResidual.clone  ( algorithm = cms.string('AK5PFchs') )
 ak5PFchsL1FastL2L3Residual  = ak5PFL1FastL2L3Residual.clone( correctors = cms.vstring('ak5PFchsL1Fastjet', 'ak5PFchsL2Relative', 'ak5PFchsL3Absolute' , 'ak5PFchsResidual') )
 
 jesChange = cms.EDProducer(
