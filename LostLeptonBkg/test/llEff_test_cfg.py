29c27eedc0315a778b9db9737993941167696a75
adraeger <>
2013-02-15 08:51:52 +0000

    *** empty log message ***

--- LostLeptonBkg/test/llEff_test_cfg.py
+++ LostLeptonBkg/test/llEff_test_cfg.py
@@ -1,22 +1,20 @@
-# $Id: llEff_test_cfg.py,v 1.1 2012/10/12 12:29:19 adraeger Exp $
+# $Id: llEff_test_cfg.py,v 1.2 2012/11/06 10:45:25 adraeger Exp $
 
-# cmsRun runMakeTreeFromPAT_cfg.py dataset=/store/user/kheine/WJetsToLNu_HT-400ToInf_8TeV-madgraph/RA2PreSelectionOnMC_HT-400ToInf_Summer12-PU_S7_START52_V9-v1_v1//956a76b9479f1eb39208c1bee6fa7dc2/RA2SkimsOnMC_10_1_Lsq.root
-import sys,os
+# cmsRun ../test/llEff_test_cfg.py dataset=/store/user/adraeger/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1_V2//9a64257873f37bce72f62fbdaab014a9/RA2Skim_100_1_DnI.root, global_tag=START53_V7F::All
 
-dataSetName = ""
-if hasattr(sys,"argv"):
-    for arg in sys.argv :
-        pairs = arg.split(',')
-        for pair in pairs:
-            vals = pair.split('=')
-            if len(vals) == 2 :
-                if vals[0] == "dataset":
-                    dataSetName = vals[1]
 
+# Read parameters
+from RA2Classic.Utils.CommandLineParams import CommandLineParams
+parameters = CommandLineParams()
+
+dataSetName = parameters.value("dataset","")
+global_tag = parameters.value("global_tag","")
+
+#global_tag="START53_V7F::All"
 print "***** SETUP ************************************"
 print "  dataSetName : "+dataSetName
+print "  global_tag : "+global_tag
 print "************************************************"
-
 
 
 
@@ -33,4 +31,5 @@ makeTreeFromPAT(process,
                 MHTMin=00.,
                 reportEveryEvt=5000,
                 testFileName=dataSetName,
+		Global_Tag=global_tag,
                 numProcessedEvt=-1)
